e48269cc1607d029b0f5a300cb0ce8ee9cd8dd28
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2018-11-10 18:01:13 +0530

    Added: A program that calculate the minimum number of change to be return in a certain denomination, given the total sum. Denomination currently used are 0, , , . Roadmap: Take the denomination from user as well

---
+++ MakeChange.java
@@ -0,0 +1,46 @@
+package com.swapnil.practice;
+
+import java.util.Scanner;
+
+public class MakeChange {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the total sum: ");
+		int sum = sc.nextInt();
+		
+		int[] output = findChange(new String[4], sum);
+		printOutput(output);
+		
+		sc.close();
+	}
+
+	private static int[] findChange(String[] changeCoins, int sum) {
+		int[] change = getChangeIntoArray(changeCoins);
+		int[] outputChange = new int[change.length];
+		
+		for (int i=0; i<change.length; i++) {
+			if ((sum / change[i] > 0) && sum > 0) {
+				outputChange[i] = sum / change[i];
+				sum = sum - (change[i] * outputChange[i]);
+			}
+		}
+		
+		return outputChange;
+	}
+
+	private static int[] getChangeIntoArray(String[] change) {
+		//Arrays.asList(change);
+		// TODO: Implement sorting logic
+		return new int[] {10,5,2,1};
+	}
+	
+	private static void printOutput(int[] output) {
+		int[] notes = new int[] {10,5,2,1};
+		
+		for (int i=0; i<output.length; i++) {
+			System.out.println("Note: " + notes[i] + " Sum: " + output[i]);
+		}
+	}
+
+}
