0f3ab839a9c70a0793e199d860d75347789cd65a
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2018-11-10 17:57:24 +0530

    Added: A programm that finds whether the two numbers are co-prime or not

---
+++ FindCoPrime.java
@@ -0,0 +1,46 @@
+package com.swapnil.practice;
+
+import java.util.Scanner;
+
+public class FindCoPrime {
+	// CoPrime numbers are numbers 
+	// that have common factor
+	// as 1 only.
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the two numbers: ");
+		String[] input = sc.nextLine().split(" ");
+		
+		try {
+			if (areCoPrime(input)) {
+				System.out.println("Yes, these numbers are co-prime indeed!");
+			} else {
+				System.out.println("These are not co-prime numbers.");
+			}
+		} catch (Exception e) {
+			System.out.println("Exception occured: " + e);
+		}
+		sc.close();
+	}
+
+	private static boolean areCoPrime(String[] input) {
+		int firstInt = Integer.parseInt(input[0]);
+		int secondInt = Integer.parseInt(input[1]);
+		
+		if (firstInt < secondInt) {
+			return areCoPrime(firstInt, secondInt);
+		} 
+		
+		return areCoPrime(secondInt, firstInt);
+	}
+
+	private static boolean areCoPrime(int smallerInt, int biggerInt) {
+		for (int i=2; i<=smallerInt/2; i++) {
+			if ((smallerInt % i  == 0) && (biggerInt % i == 0)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
