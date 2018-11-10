0a097a2f54937f4ea8358416f4db9f1d91f92e72
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2018-11-10 17:58:51 +0530

    Added: A program that finds the lowest common multiple of the two input numbers

---
+++ FindLCM.java
@@ -0,0 +1,43 @@
+package com.swapnil.practice;
+
+import java.util.Scanner;
+
+public class FindLCM {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your numbers: ");
+		String[] input = sc.nextLine().split(" ");
+		int lcm = findLcm(input);
+		
+		System.out.println("L.C.M is: " + lcm);
+		
+		sc.close();
+	}
+	
+	private static int findLcm(String[] input) {
+		int firstInt = Integer.parseInt(input[0]);
+		int secondInt = Integer.parseInt(input[1]);
+		
+		if (firstInt < secondInt) {
+			return findLcm(firstInt, secondInt);
+		}
+		
+		return findLcm(secondInt, firstInt);
+	}
+
+	private static int findLcm(int firstInt, int secondInt) {
+		return (firstInt * secondInt) / findGcd(firstInt, secondInt);
+	}
+
+	private static int findGcd(int smallerInt, int biggerInt) {
+		for (int i=smallerInt; i>0; i--) {
+			if ((smallerInt % i == 0) && (biggerInt % i == 0)) {
+				return i;
+			}
+		}
+		
+		return 1;
+	}
+
+}
