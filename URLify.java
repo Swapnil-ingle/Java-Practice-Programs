3ea63be30ba57b5de8d7678e131961589c3b58ce
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-05-31 20:23:47 +0530

    SI: Added solution for CtCi C1:Ex3

---
+++ URLify.java
@@ -0,0 +1,37 @@
+
+
+/**
+ * 
+ * @author Swap
+ * 
+ * Solution for 'Cracking The Coding Interview' Chapter 1 - Ex#3
+ * 
+ * URLify(input, len)
+ *
+ */
+public class URLify {
+
+	public static void main(String[] args) {
+		String input = "Mr John Smith    ";
+		System.out.println(urlify(input.toCharArray(), 13));
+	}
+	
+	private static char[] urlify(char[] input, int len) {
+		int lenNew = input.length - 1;
+		int j = len - 1;
+		
+		for (int i = lenNew; i >= 0; i--) {
+			if (input[j] == ' ') {
+				input[i] = '0';
+				input[--i] = '2';
+				input[--i] = '%';				
+			} else {
+				input[i] = input[j];
+			}
+			j--;
+		}
+		
+		return input;
+	}
+
+}
