18520b9e658bdf5f20397f02e408e3f55dded5b2
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-06-08 20:17:00 +0530

    SI: Solved C1Ex7 of CtCi book

---
+++ com/swapnil/java/practice/RotateNbyNMatrix.java
@@ -0,0 +1,47 @@
+package com.swapnil.java.practice;
+
+public class RotateNbyNMatrix {
+
+	public static void main(String[] args) {
+		int[][] inputMatrix = new int[3][3];
+		inputMatrix[0][0] = 1;
+		inputMatrix[0][1] = 2;
+		inputMatrix[0][2] = 3;
+		inputMatrix[1][0] = 4;
+		inputMatrix[1][1] = 5;
+		inputMatrix[1][2] = 6;
+		inputMatrix[2][0] = 7;
+		inputMatrix[2][1] = 8;
+		inputMatrix[2][2] = 9;
+
+		System.out.println("=========== Before rotation =========== ");
+		printMatrix(inputMatrix);
+		System.out.println("=========== After rotation =========== ");
+		printMatrix(rotateMatrix(inputMatrix));
+	}
+	
+	private static int[][] rotateMatrix(int[][] input) {
+		int n = input[0].length;
+		int[][] newMatrix = new int[n][n];
+		
+		for (int r = 0; r < n; r++) {
+			for (int c = 0; c < n; c++) {
+				int ele = input[r][c];
+				newMatrix[n - (c + 1)][r] = ele;
+			}
+		}
+		
+		return newMatrix;
+	}
+	
+	private static void printMatrix(int[][] matrix) {
+		int n = matrix[0].length;
+
+		for (int r = 0; r < n; r++) {
+			for (int c = 0; c < n; c++) {
+				System.out.print(" | " + matrix[r][c]);
+			}
+			System.out.println(" | ");
+		}
+	}
+}
